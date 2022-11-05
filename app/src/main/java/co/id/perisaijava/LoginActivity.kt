a16292aca6d7b294f0e8ce9f85c62731345d653f
Khaerul Umam <ukhaerul8@gmail.com>
2022-11-05 13:40:21 +0700

    perbaikan layout login dan consume login gmail & maps

--- app/src/main/java/co/id/perisaijava/LoginActivity.kt
+++ app/src/main/java/co/id/perisaijava/LoginActivity.kt
@@ -5,44 +5,43 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.net.ConnectivityManager
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatTextView
+import co.id.perisaijava.databinding.ActivityLoginBinding
 import com.androidnetworking.AndroidNetworking
 import com.androidnetworking.common.Priority
 import com.androidnetworking.error.ANError
 import com.androidnetworking.interfaces.JSONObjectRequestListener
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import org.json.JSONException
 import org.json.JSONObject
 
 class LoginActivity : AppCompatActivity() {
-    var etEmail: EditText? = null
-    var etPassword:EditText? = null
-    var btnLogin: AppCompatTextView? = null
-    var btnRegis:AppCompatTextView? = null
     var conMgr: ConnectivityManager? = null
     var id: String? = null
-    var email:kotlin.String? = null
-    var name:kotlin.String? = null
-    var chance:kotlin.String? = null
-    var level:kotlin.String? = null
-    var score:kotlin.String? = null
     var pDialog: ProgressDialog? = null
     var sharedpreferences: SharedPreferences? = null
     var session = false
     val my_shared_preferences = "my_shared_preferences"
     val session_status = "session_status"
+    private lateinit var activityLoginBinding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
+        activityLoginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(activityLoginBinding.root)
+        initListeners()
         conMgr = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
         {
             if (conMgr!!.activeNetworkInfo != null && conMgr!!.activeNetworkInfo!!.isAvailable
                 && conMgr!!.activeNetworkInfo!!.isConnected
             ) {
+
             } else {
                 Toast.makeText(
                     applicationContext, "No Internet Connection",
@@ -51,104 +50,103 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
-        etEmail = findViewById(R.id.etEmail)
-        etPassword = findViewById(R.id.etPassword)
-        btnLogin = findViewById(R.id.btn_login)
-        btnRegis = findViewById(R.id.btn_register)
-
         // Cek session login jika TRUE maka langsung buka MainActivity
 
         // Cek session login jika TRUE maka langsung buka MainActivity
         sharedpreferences = getSharedPreferences(my_shared_preferences, MODE_PRIVATE)
         session = sharedpreferences!!.getBoolean(session_status, false)
         id = sharedpreferences!!.getString("id", null)
-        email = sharedpreferences!!.getString("email", null)
-        name = sharedpreferences!!.getString("name", null)
-        chance = sharedpreferences!!.getString("chance", null)
-        level = sharedpreferences!!.getString("level", null)
-        score = sharedpreferences!!.getString("score", null)
 
-        if (session) {
-            val intent = Intent(this@LoginActivity, MainActivity::class.java)
-            intent.putExtra("id", id)
-            intent.putExtra("email", email)
-            intent.putExtra("name", name)
-            intent.putExtra("chance", chance)
-            intent.putExtra("level", level)
-            startActivity(intent)
+        if (isUserSignedIn()){
+            var pindah = Intent(this@LoginActivity,MainActivity::class.java)
+            startActivity(pindah)
             finish()
         }
 
-        btnRegis!!.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
-            startActivity(intent)
-        })
-
-        btnLogin!!.setOnClickListener(View.OnClickListener {
-            val email = etEmail!!.getText().toString()
-            val password = etPassword!!.getText().toString()
-
-            // mengecek kolom yang kosong
-            if (email.trim { it <= ' ' }.length > 0 && password.trim { it <= ' ' }.length > 0) {
-                if (conMgr!!.activeNetworkInfo != null && conMgr!!.activeNetworkInfo!!.isAvailable
-                    && conMgr!!.activeNetworkInfo!!.isConnected
-                ) {
-                    checkLogin(email, password)
-                } else {
-                    Toast.makeText(applicationContext, "No Internet Connection", Toast.LENGTH_LONG)
-                        .show()
-                }
-            } else {
-                // Prompt user to enter credentials
-                Toast.makeText(applicationContext, "Kolom tidak boleh kosong", Toast.LENGTH_LONG)
-                    .show()
-            }
-        })
     }
-    private fun checkLogin(email: String, password: String) {
-        var pindah = Intent(this@LoginActivity,MainActivity::class.java)
-        startActivity(pindah)
-//        pDialog = ProgressDialog(this)
-//        pDialog!!.setCancelable(false)
-//        pDialog!!.setMessage("Logging in ...")
-//        showDialog()
-//        AndroidNetworking.post(Server.ENDPOINT_LOGIN)
-//            .addBodyParameter("email", email)
-//            .addBodyParameter("password", password)
-//            .setPriority(Priority.HIGH)
-//            .build()
-//            .getAsJSONObject(object : JSONObjectRequestListener {
-//                override fun onResponse(response: JSONObject) {
-////                        setIsLoading(false);
-//                    println("Response $response")
-//                    val editor = sharedpreferences!!.edit()
-//                    editor.putBoolean(LoginActivity.session_status, true)
-//                    try {
-//                        editor.putString("id", response.getString("id"))
-//                        editor.putString("name", response.getString("name"))
-//                        editor.putString("chance", response.getString("chance"))
-//                        editor.putString("score", response.getString("score"))
-//                        editor.putString("level", response.getString("level"))
-//                    } catch (e: JSONException) {
-//                        e.printStackTrace()
-//                    }
-//                    editor.putString("email", email)
-//                    editor.commit()
-//
-//                    // Memanggil main activity
-//                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
-//                    intent.putExtra("id", id)
-//                    intent.putExtra("email", email)
-//                    hideDialog()
-//                    startActivity(intent)
-//                    finish()
-//                }
-//
-//                override fun onError(anError: ANError) {
-//                    println("Erorr Login")
-//                    hideDialog()
-//                }
-//            })
+
+    private fun initListeners() {
+
+        activityLoginBinding.signInButton.setOnClickListener {
+            loginGoogle()
+        }
+
+//        activityMainBinding.tvSignOut.setOnClickListener {
+//            signout()
+//        }
+
+    }
+
+    private fun loginGoogle() {
+        val signInIntent = getGoogleSinginClient().signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    //region User Google Sign-in and sign-out Code
+
+    fun getGoogleSinginClient() : GoogleSignInClient {
+        /**
+         * Configure sign-in to request the user's ID, email address, and basic
+         * profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+         */
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestProfile()
+            .build()
+
+
+        /**
+         * Build a GoogleSignInClient with the options specified by gso.
+         */
+        return GoogleSignIn.getClient(this, gso);
+    }
+
+    private fun isUserSignedIn(): Boolean {
+
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        return account != null
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            var pindah = Intent(this@LoginActivity,MainActivity::class.java)
+            startActivity(pindah)
+            finish()
+//            handleSignData(data)
+
+        }
+    }
+
+    private fun handleSignData(data: Intent?) {
+        // The Task returned from this call is always completed, no need to attach
+        // a listener.
+        GoogleSignIn.getSignedInAccountFromIntent(data)
+            .addOnCompleteListener {
+                "isSuccessful ${it.isSuccessful}".print()
+                if (it.isSuccessful){
+                    // user successfully logged-in
+                    "account ${it.result?.account}".print()
+                    "displayName ${it.result?.displayName}".print()
+                    "Email ${it.result?.email}".print()
+                } else {
+                    // authentication failed
+                    "exception ${it.exception}".print()
+                }
+            }
+
+    }
+
+    companion object{
+        const val RC_SIGN_IN = 0
+        const val TAG_KOTLIN = "TAG_KOTLIN"
+    }
+    fun Any.print(){
+        Log.v(TAG_KOTLIN, " $this")
     }
 
     private fun showDialog() {
--- app/src/main/java/co/id/perisaijava/MainActivity.kt
+++ app/src/main/java/co/id/perisaijava/MainActivity.kt
@@ -1,21 +1,193 @@
 package co.id.perisaijava
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
-import co.id.perisaijava.R
+import androidx.core.app.ActivityCompat
+import co.id.perisaijava.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.location.*
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
-    private var btnCard :CardView ?= null
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val LOCATION_PERMS = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+    private var wayLatitude = 0.0
+    private  var wayLongitude= 0.0
+
+    private val isContinue = false
+    private var locationRequest: LocationRequest? = null
+    private var locationCallback: LocationCallback? = null
+    private val LOCATION_REQUEST: Int = 1340
+    private var geoCoder: Geocoder?=null
+    private lateinit var activityMainActivity: ActivityMainBinding
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        btnCard = findViewById(R.id.cardSafeHouse)
-
-        btnCard!!.setOnClickListener {
+        activityMainActivity = ActivityMainBinding.inflate(layoutInflater)
+        geoCoder = Geocoder(this, Locale.getDefault())
+        setContentView(activityMainActivity.root)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationRequest = LocationRequest.create()
+        locationRequest!!.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest!!.interval = 20 * 1000
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                if (locationResult == null) {
+                    return
+                }
+                for (location in locationResult.locations) {
+                    if (location != null) {
+                        wayLatitude = location.latitude
+                        wayLongitude = location.longitude
+                    }
+                }
+            }
+        }
+        if (canAccessLocation()) {
+            loadLocationNow()
+        }
+        else {
+            requestPermissions(LOCATION_PERMS, LOCATION_REQUEST)
+        }
+        loadDataProfile()
+        activityMainActivity.cardSafeHouse.setOnClickListener {
             var pindah = Intent(this@MainActivity,MapsActivity::class.java)
             startActivity(pindah)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1000 -> if (canAccessLocation()) {
+                if (grantResults.isNotEmpty()
+                    && grantResults[0] === PackageManager.PERMISSION_GRANTED
+                ) {
+                    if (isContinue) {
+                        loadLocationNow()
+                    } else {
+                       loadLocationNow()
+                    }
+                } else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                requestPermissions(LOCATION_PERMS, LOCATION_REQUEST)
+            }
+        }
+    }
+
+    private fun loadLocationNow() {
+        if (ActivityCompat.checkSelfPermission(
+                this@MainActivity,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                this@MainActivity,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+               1000
+            )
+        } else {
+        if (isContinue) {
+            fusedLocationClient.requestLocationUpdates(locationRequest!!, locationCallback!!, null)
+        } else {
+            fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this@MainActivity) { location ->
+                    if (location != null) {
+                        wayLatitude = location.getLatitude()
+                        wayLongitude = location.getLongitude()
+                        println("Lat :" + location!!.latitude)
+                        println("Long :" + location.longitude)
+                        try {
+            var addresses =
+                geoCoder!!.getFromLocation(wayLatitude, wayLongitude, 1);
+            if (addresses.isEmpty()) {
+                activityMainActivity.txtLocation.setText("Waiting for Location");
+            } else {
+                activityMainActivity.txtLocation.setText(
+                   addresses.get(0)
+                        .getLocality() + ", " + addresses.get(0)
+                        .getAdminArea()
+                );
+            }
+        } catch (e: java.lang.Exception) {
+            println("Catch " + e.message)
+        }
+                    } else {
+                        fusedLocationClient.requestLocationUpdates(
+                            locationRequest!!,
+                            locationCallback!!,
+                            null
+                        )
+                    }
+                }
+        }
+        }
+
+
+//
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun canAccessLocation(): Boolean {
+        return hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun hasPermission(perm: String): Boolean {
+        return PackageManager.PERMISSION_GRANTED == checkSelfPermission(perm)
+    }
+    private fun loadDataProfile() {
+        val dateFormat: DateFormat = SimpleDateFormat("HH")
+        val date = Date()
+        var waktuSaatIni = dateFormat.format(date)
+        println("Waktu Saat ini "+dateFormat.format(date))
+        if (waktuSaatIni.toInt() in 5..11){
+            activityMainActivity.txtSelamat.text = "Selamat Pagi"
+        } else if (waktuSaatIni.toInt() in 11..14){
+            activityMainActivity.txtSelamat.text = "Selamat Siang"
+        }else if (waktuSaatIni.toInt() in 15..17){
+            activityMainActivity.txtSelamat.text = "Selamat Sore"
+        } else {
+            activityMainActivity.txtSelamat.text = "Selamat Malam"
+        }
+        val acct = GoogleSignIn.getLastSignedInAccount(this)
+        if (acct != null) {
+            val personName = acct.displayName
+            val personGivenName = acct.givenName
+            val personFamilyName = acct.familyName
+            val personEmail = acct.email
+            val personId = acct.id
+            val personPhoto: Uri? = acct.photoUrl
+
+            activityMainActivity.txtName.text = "Halo, $personName"
+            activityMainActivity.avatar.setImageURI(personPhoto)
         }
     }
 }
