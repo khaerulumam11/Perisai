1cb669cc78882361538c2c9ce3164b597412ce52
Khaerul Umam <ukhaerul8@gmail.com>
2022-11-10 02:40:12 +0700

    - penambahan fungsi more marker (done) - penambahan dialog for detail location safehouse (done)

--- app/src/main/java/co/id/perisaijava/MapsAllActivity.kt
+++ app/src/main/java/co/id/perisaijava/MapsAllActivity.kt
@@ -1,25 +1,57 @@
 package co.id.perisaijava
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.location.Geocoder
+import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import co.id.perisaijava.databinding.ActivityMapsAllBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.*
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import java.util.*
 
-class MapsAllActivity : AppCompatActivity() , OnMapReadyCallback {
+
+class MapsAllActivity : AppCompatActivity() , OnMapReadyCallback, OnMarkerClickListener {
     private var maps: GoogleMap? = null
     private lateinit var activityMapsAllBinding: ActivityMapsAllBinding
+    private var dialogLayout: BottomSheetDialog?=null
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val LOCATION_PERMS = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+    private var geoCoder: Geocoder?=null
+    private var listLatLng = mutableListOf<LatLng>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMapsAllBinding = ActivityMapsAllBinding.inflate(layoutInflater)
         setContentView(activityMapsAllBinding.root)
+        geoCoder = Geocoder(this, Locale.getDefault())
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         val mapFragment = (supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment?)!!
         mapFragment!!.getMapAsync(this)
-
+        showDialog()
+        listLatLng.add(LatLng(-7.328712, 110.510710))
+        listLatLng.add(LatLng(-7.328257, 110.505658))
+        listLatLng.add(LatLng(-7.3303195,110.5034887))
+        listLatLng.add(LatLng(-7.335622, 110.503820))
         activityMapsAllBinding.toolbar.setTitle(resources.getString(R.string.safehosue))
         activityMapsAllBinding.toolbar.setNavigationIcon(R.drawable.ic_baseline_chevron_left_grey_24)
         activityMapsAllBinding.toolbar.setNavigationOnClickListener {
@@ -27,30 +59,148 @@ class MapsAllActivity : AppCompatActivity() , OnMapReadyCallback {
         }
     }
 
+    fun showDialog() {
+        dialogLayout= BottomSheetDialog(this)
+        dialogLayout!!.setContentView(R.layout.dialog_info_safehouse)
+    }
     override fun onMapReady(googleMap: GoogleMap?) {
         maps = googleMap
         // Position the map's camera near Alice Springs in the center of Australia,
         // and set the zoom factor so most of Australia shows on the screen.
+        for (i in  0 until listLatLng.size) {
 
-//        Polygon polygon2 = googleMap.addPolygon(new PolygonOptions()
-//                .clickable(true)
-//                .add(
-//                        new LatLng(-31.673, 128.892),
-//                        new LatLng(-31.952, 115.857),
-//                        new LatLng(-17.785, 122.258),
-//                        new LatLng(-12.4258, 130.7932)));
-//        polygon2.setTag("beta");
-//        stylePolygon(polygon2);
-        // [END_EXCLUDE]
+            var marker :Marker?=null
+            marker = googleMap!!.addMarker(MarkerOptions().position(listLatLng.get(i)).title("Safehouse #" + (i+1)).icon(BitmapFromVector(this@MapsAllActivity,R.drawable.ic_baseline_home_pink_24)))
+            marker.tag = i
+            // below line is use to add marker to each location of our array list.
 
-        // Position the map's camera near Alice Springs in the center of Australia,
-        // and set the zoom factor so most of Australia shows on the screen.
-        googleMap!!.moveCamera(
-            CameraUpdateFactory.newLatLngZoom(
-                LatLng(-7.3364893,110.5014054),
-                15f
-            )
-        )
+            // below lin is use to zoom our camera on map.
+            googleMap.animateCamera(CameraUpdateFactory.zoomTo(18.0f))
+
+            // below line is use to move our camera to the specific location.
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(listLatLng.get(i),15f))
+        }
+
+        maps!!.setOnMarkerClickListener(this)
 
     }
+
+    private fun BitmapFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
+        // below line is use to generate a drawable.
+        val vectorDrawable = ContextCompat.getDrawable(context, vectorResId)
+
+        // below line is use to set bounds to our vector drawable.
+        vectorDrawable!!.setBounds(
+            0,
+            0,
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight
+        )
+
+        // below line is use to create a bitmap for our
+        // drawable which we have added.
+        val bitmap = Bitmap.createBitmap(
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+
+        // below line is use to add bitmap in our canvas.
+        val canvas = Canvas(bitmap)
+
+        // below line is use to draw our
+        // vector drawable in canvas.
+        vectorDrawable.draw(canvas)
+
+        // after generating our bitmap we are returning our bitmap.
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+
+    override fun onMarkerClick(marker: Marker?): Boolean {
+        for (i in  0 until listLatLng.size) {
+            if (marker!!.tag == i){
+                openDialog(i)
+            }
+        }
+        return false
+    }
+
+    private fun openDialog(i: Int) {
+        val txtTitle= dialogLayout!!.findViewById<TextView>(R.id.txtTitle)
+        txtTitle!!.text = "Safehouse Warga #"+(i+1)
+        val btnDirection= dialogLayout!!.findViewById<CardView>(R.id.btnDirection)
+        btnDirection!!.setOnClickListener {
+            getDirection(i)
+        }
+        loadLocationNow(i)
+        dialogLayout!!.show()
+    }
+
+    private fun getDirection(a: Int) {
+        var latLng:LatLng ?=null
+        for (i in  0 until listLatLng.size) {
+            if (i == a){
+                latLng = listLatLng[i]
+            }
+        }
+
+        var finalLatLng = "google.navigation:q="+latLng!!.latitude.toString()+","+latLng.longitude.toString()
+        val gmmIntentUri = Uri.parse(finalLatLng)
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+        mapIntent.setPackage("com.google.android.apps.maps")
+        startActivity(mapIntent)
+    }
+
+    private fun loadLocationNow(a:Int) {
+        if (ActivityCompat.checkSelfPermission(
+                this@MapsAllActivity,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                this@MapsAllActivity,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this@MapsAllActivity,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                1000
+            )
+        } else {
+                fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this@MapsAllActivity) { location ->
+                        if (location != null) {
+                            try {
+                                for (i in  0 until listLatLng.size) {
+                                    if (a == i) {
+                                        var addresses =
+                                            geoCoder!!.getFromLocation(
+                                                listLatLng.get(i).latitude,
+                                                listLatLng.get(i).longitude,
+                                                1
+                                            );
+                                        if (addresses.isEmpty()) {
+                                            dialogLayout!!.findViewById<TextView>(R.id.txtAlamat)!!.setText("Waiting for Location");
+                                        } else {
+                                            dialogLayout!!.findViewById<TextView>(R.id.txtAlamat)!!.setText(
+                                                addresses.get(0).getAddressLine(0)
+                                            );
+                                        }
+                                    }
+                                }
+                            } catch (e: java.lang.Exception) {
+                                println("Catch " + e.message)
+                            }
+                        }
+                    }
+
+        }
+
+
+//
+    }
+
 }
