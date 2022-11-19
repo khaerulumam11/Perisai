f08c9112ccb9cbf5d00b7c04820f3485b0716658
Khaerul Umam <ukhaerul8@gmail.com>
2022-11-19 09:54:00 +0700

    - perbaikan content (done) - penambahan fitur edit profile (done) - perhitungan jarak (done)

--- app/src/main/java/co/id/perisaijava/model/SafehouseModel.kt
+++ app/src/main/java/co/id/perisaijava/model/SafehouseModel.kt
@@ -1,52 +1,18 @@
-package co.id.perisaijava.model;
+package co.id.perisaijava.model
 
-import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLng
 
-public class SafehouseModel {
-    private String name;
-    private LatLng latLng;
-    private String alamat;
-    private String foto;
+class SafehouseModel {
+    var name: String? = null
+    var latLng: LatLng? = null
+    var alamat: String? = null
+    var foto: String? = null
 
-    public SafehouseModel() {
-    }
-
-    public SafehouseModel(String name, LatLng latLng, String alamat, String foto) {
-        this.name = name;
-        this.latLng = latLng;
-        this.alamat = alamat;
-        this.foto = foto;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LatLng getLatLng() {
-        return latLng;
-    }
-
-    public void setLatLng(LatLng latLng) {
-        this.latLng = latLng;
-    }
-
-    public String getAlamat() {
-        return alamat;
-    }
-
-    public void setAlamat(String alamat) {
-        this.alamat = alamat;
-    }
-
-    public String getFoto() {
-        return foto;
-    }
-
-    public void setFoto(String foto) {
-        this.foto = foto;
+    constructor() {}
+    constructor(name: String?, latLng: LatLng?, alamat: String?, foto: String?) {
+        this.name = name
+        this.latLng = latLng
+        this.alamat = alamat
+        this.foto = foto
     }
 }
